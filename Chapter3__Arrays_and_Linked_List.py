8ecff472785e1e4981cbb28f7a5fe25916b620b5
Sai Tejas Janjur <74515547+saitejas-janjur@users.noreply.github.com>
2021-11-08 00:16:15 -0600

    LinkedList code examples

--- Chapter3__Arrays_and_Linked_List.py
+++ Chapter3__Arrays_and_Linked_List.py
@@ -1,9 +1,9 @@
-# Memory of a computer
+# Arrays and Linked List
+
+# Memory of a computer -
 
 # -> Each time you wanna store an item in memory, you ask the computer for some space, the computer gives you a location where you can sore that item.
 # -> If you have to store multiple items, the 2 basic way of storing them are - Arrays and Lists.
-
-# Arrays and Linked List -
 
 # How memory works when using ARRAYS  -
 
@@ -80,6 +80,96 @@
 # O(1) -> Constant time
 # O(n) -> Linear time
 
+class Node:
+    def __init__(self, data = None, next = None):
+        self.data = data
+        self.next = next
+class LinkedList:
+    def __init__(self):
+        self.head = None
+    def insert_at_begining(self, data):
+        node = Node(data, self.head)
+        self.head = node
+
+    def print(self):
+        if self.head is None:
+            print("Linked List is empty")
+        itr = self.head
+        llstr = ''
+
+        while itr:
+            llstr +=  str(itr.data) + '---->'
+            itr = itr.next
+        print(llstr)
+
+    def insert_at_end(self, data):
+        if self.head is None:
+            self.head = Node(data, None)
+            return
+
+        itr = self.head
+        while itr.next:
+            itr = itr.next
+        itr.next = Node(data, None)
+
+    def insert_values(self, data_list):
+        self.head = None
+        for data in data_list:
+            self.insert_at_end(data)
+
+    def get_legth(self):
+        count = 0
+        itr = self.head
+        while itr:
+            count += 1
+            itr = itr.next
+        return count
+
+    def remove_at(self, index):
+        if index < 0 or index >= self.get_legth():
+            raise Exception("Invalid index")
+
+        if index == 0:
+            self.head = self.head.next
+            return
+        count = 0
+        itr = self.head
+        while itr:
+            if count == index - 1:
+                itr.next = itr.next.next
+                break
+            itr = itr.next
+            count += 1
+    def insert_at(self, index, data):
+        if index < 0 or index > self.get_legth():
+            raise Exception ("Invalid Index")
+        if index == 0:
+            self.insert_at_begining(data)
+            return
+        count = 0
+        itr = self.head
+        while itr:
+            if count == index - 1:
+                node = Node(data, itr.next)
+                itr.next = node
+                break
+            itr = itr.next
+            count += 1
+
+
+if __name__ == '__main__':
+    ll = LinkedList()
+    # ll.insert_at_begining(50)
+    # ll.insert_at_begining(48)
+    # ll.insert_at_begining(2)
+    # ll.insert_at_begining(8)
+    # ll.insert_at_end(5)
+    # ll.insert_at_end(511)
+    ll.insert_values([5,8,4,3,6])
+    ll.print()
+    # ll.get_legth()
+    ll.remove_at(3)
+    ll.print()
 
 
 
